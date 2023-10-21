80a6ad7650577d4c418cd59e2820b201a8615256
AnikaGG <anikug@hotmail.com>
2023-10-21 16:08:54 -0700

    scoredboard AND we get acorns from left and right!

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,18 @@ use winit::{
 use std::path::Path;
 use imageproc::rect::Rect;
 use rusttype::{Font, Scale};
+mod game_state;
 mod input;
 mod animation;
 use rand::Rng;
+use bytemuck::{Pod, Zeroable};
+use glyphon::{
+    Attrs, Buffer, Color, Family, FontSystem, Metrics, Resolution, Shaping, SwashCache, TextArea,
+    TextAtlas, TextBounds, TextRenderer,
+};
+use wgpu::{
+    CompositeAlphaMode, MultisampleState, 
+};
 
 // AsRef means we can take as parameters anything that cheaply converts into a Path,
 // for example an &str.
@@ -82,26 +91,21 @@ impl Character {
             self.screen_region[0] -= self.speed;
         }
     }
-
     fn face_left(&mut self) {
         self.facing_right = false;
-        // TODO: FLIP SPRITE
         if self.screen_region[2] < 0.0 {
             self.screen_region[2] *= -1.0;
-            self.screen_region[0] -= 250.0;
+            self.screen_region[0] -= 60.0;
         }
         
     }
-
     fn face_right(&mut self) {
         self.facing_right = true;
-        // TODO: FLIP SPRITE
         if self.screen_region[2] > 0.0 {
             self.screen_region[2] *= -1.0;
-            self.screen_region[0] += 250.0;
+            self.screen_region[0] += 60.0;
         }
     }
-
     fn move_down(&mut self) {
         self.screen_region[1] -= self.speed;
 
@@ -110,7 +114,6 @@ impl Character {
             self.screen_region[0] = rand::thread_rng().gen_range(0..1025) as f32;
         }
     }
-
     fn reset_y(&mut self){
         self.screen_region[1] = 768.0;
         self.screen_region[0] = rand::thread_rng().gen_range(0..1025) as f32;
@@ -122,6 +125,9 @@ impl Character {
 // the browser.  On desktop, we'll just be running this function to completion.
 async fn run(event_loop: EventLoop<()>, window: Window) {
     let size = window.inner_size();
+
+    let mut gs = game_state::init_game_state();
+    gs.typing = true;
 
     // An Instance is an instance of the graphics API.  It's the context in which other
     // WGPU values and operations take place, and there can be only one.
@@ -190,11 +196,38 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         format: swapchain_format,
         width: size.width,
         height: size.height,
+        // present_mode: wgpu::PresentMode::Fifo,
         present_mode: wgpu::PresentMode::Fifo,
-        alpha_mode: swapchain_capabilities.alpha_modes[0],
+        // alpha_mode: swapchain_capabilities.alpha_modes[0],
+        alpha_mode: CompositeAlphaMode::Opaque,
         view_formats: vec![],
     };
     surface.configure(&device, &config);
+
+    //new
+    let mut original_text = "Hello world! 👋\nThis is rendered with 🦅 glyphon 🦁\nThe text below should be partially clipped.\na b c d e f g h i j k l m n o p q r s t u v w x y z";
+    let num_chars = original_text.len() as u32;
+    let mut displayed_text:String = String::from("");
+    // <li>Add/edit a set_text line before the run call, to set the buffer text to be the displayed text, which is currently an empty String.</li>
+    // Set up text renderer
+    let mut font_system = FontSystem::new();
+    let mut cache = SwashCache::new();
+    let mut atlas = TextAtlas::new(&device, &queue, swapchain_format);
+    let mut text_renderer = TextRenderer::new(&mut atlas, &device, MultisampleState::default(), None);
+    let mut buffer = Buffer::new(&mut font_system, Metrics::new(60.0, 42.0));
+    
+    let physical_width = (size.width as f64 * window.scale_factor()) as f32;
+    let physical_height = (size.height as f64 * window.scale_factor()) as f32;
+    
+    buffer.set_size(&mut font_system, physical_width, physical_height);
+
+    let score_text = format!("Score: {}", gs.score);
+    // buffer.set_text(&mut font_system, "Hello world! 👋\nThis is rendered with 🦅 glyphon 🦁\nThe text below should be partially clipped.\na b c d e f g h i j k l m n o p q r s t u v w x y z", Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+    // buffer.set_text(&mut font_system, &displayed_text, Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+    // buffer.set_text(&mut font_system, &gs.score.to_string(), Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+    buffer.set_text(&mut font_system, &score_text, Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+    // <li>Create a file called game_state.rs, and add the following code. This will serve as a game state object that we can make calls to, see the variables of, and edit the variables of from any files. Although we could make these variables in the original main function, in more complicated games, it will be beneficial to have a gamestate, and these are some variables regarding text that make sense to go inside this class.</li>
+    buffer.shape_until_scroll(&mut font_system);
 
     // Load the shaders from disk.  Remember, shader programs are things we compile for
     // our GPU so that it can compute vertices and colorize fragments.
@@ -386,44 +419,45 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         screen_size: [1024.0, 768.0],
     };
 
-    // total squirrel is 52x260px with 5 frames
-    // one frame of squirrel is 52x52px
+    // total squirrel is 36x133px with 6 frames
+    // one frame of squirrel is 36x22px
     let sprite_sheet_dimensions = squirrel_img.dimensions();
-    let squirrel_total_w: f32 = 52.0;
-    let squirrel_total_h: f32 = 260.0;
-    let squirrel_frame_w: f32 = 52.0;
-    let squirrel_frame_h: f32 = 52.0;
+    let squirrel_total_w: f32 = 35.0;
+    let squirrel_total_h: f32 = 174.0;
+    let squirrel_frame_w: f32 = 35.0;
+    let squirrel_frame_h: f32 = 22.5;
 
     // frames will be a series of frames 
     let mut squirrel_sheet_positions: Vec<[f32; 4]> = vec![
+
         // frame 1 sheet position
-        [259.0/sprite_sheet_dimensions.0 as f32, (1.0 * squirrel_frame_h)/squirrel_total_h, 1.0 - 259.0/sprite_sheet_dimensions.0 as f32, squirrel_frame_h/squirrel_total_h],
+        [126.0/162.0, 25.0/174.0, 32.0/162.0, 21.0/174.0],
 
         // frame 2 sheet position
-        [259.0/sprite_sheet_dimensions.0 as f32, (2.0 * squirrel_frame_h)/squirrel_total_h, 1.0 - 259.0/sprite_sheet_dimensions.0 as f32, squirrel_frame_h/squirrel_total_h],
+        [126.0/162.0, 48.0/174.0, 32.0/162.0, 22.0/174.0],
  
         // frame 3 sheet position
-        [259.0/sprite_sheet_dimensions.0 as f32, (3.0 * squirrel_frame_h)/squirrel_total_h, 1.0 - 259.0/sprite_sheet_dimensions.0 as f32, squirrel_frame_h/squirrel_total_h],
+        [126.0/162.0, 72.0/174.0, 28.0/162.0, 23.0/174.0],
 
         // frame 4 sheet position
-        [259.0/sprite_sheet_dimensions.0 as f32, (4.0 * squirrel_frame_h)/squirrel_total_h, 1.0 - 259.0/sprite_sheet_dimensions.0 as f32, squirrel_frame_h/squirrel_total_h],
+        [126.0/162.0, 97.0/174.0, 35.0/162.0, 23.0/174.0],
 
         // frame 5 sheet position
-        [259.0/sprite_sheet_dimensions.0 as f32, (5.0 * squirrel_frame_h)/squirrel_total_h, 1.0 - 259.0/sprite_sheet_dimensions.0 as f32, squirrel_frame_h/squirrel_total_h],
+        [126.0/162.0, 122.0/174.0, 33.0/162.0, 22.0/174.0],
 
     ];
 
     let mut sprites: Vec<GPUSprite> = vec![
         // SQUIRREL
     GPUSprite {
-        screen_region: [32.0, 32.0, 208.0, 208.0],
+        screen_region: [32.0, 32.0, 100.0, 100.0],
         sheet_region: squirrel_sheet_positions[0],   
     },
 
         // NUT
     GPUSprite {
-        screen_region: [32.0, 200.0, 84.73, 84.73],
-        sheet_region: [0.0, 0.0, 258.0/sprite_sheet_dimensions.0 as f32, 1.0],   
+        screen_region: [20.0, 200.0, 55.0, 55.0],
+        sheet_region: [0.0, 0.0, 123.0/sprite_sheet_dimensions.0 as f32, 172.0/sprite_sheet_dimensions.1 as f32],   
     }
     ];
 
@@ -494,6 +528,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     event_loop.run(move |event, _, control_flow| {
         // By default, tell the windowing system that there's no more work to do
         // from the application's perspective.
+        // *control_flow = ControlFlow::Poll;
         *control_flow = ControlFlow::Poll;
         // Depending on the event, we'll need to do different things.
         // There is some pretty fancy pattern matching going on here,
@@ -523,8 +558,49 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 queue.write_buffer(&buffer_sprite, 0, bytemuck::cast_slice(&sprites));
                 // ...all the drawing stuff goes here...
                 window.request_redraw();
+
+                // if gs.typing{
+                    // let chars_iter = original_text.chars();
+                    // for char in chars_iter.skip(gs.chars_typed as usize){
+                    //     displayed_text += &char.to_string();
+                    //     break;
+                    // }
+                    // buffer.set_text(&mut font_system, &displayed_text, Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+                    // buffer.set_text(&mut font_system, &gs.score.to_string(), Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+                    // ADD TYPING LOGIC
+                    // gs.chars_typed += 1;
+                    // if gs.chars_typed == num_chars{
+                    //     gs.typing = false;
+                    // }
+                // }
+
                 // Leave now_keys alone, but copy over all changed keys
                 input.next_frame();
+
+                text_renderer.prepare(
+                    &device,
+                    &queue,
+                    &mut font_system,
+                    &mut atlas,
+                    Resolution {
+                        width: config.width,
+                        height: config.height,
+                    },
+                    [TextArea {
+                        buffer: &buffer,
+                        left: 10.0,
+                        top: 10.0,
+                        scale: 1.0,
+                        bounds: TextBounds {
+                            left: 0,
+                            top: 0,
+                            right: 600,
+                            bottom: 160,
+                        },
+                        default_color: Color::rgb(255, 255, 255),
+                    }],
+                    &mut cache,
+                ).unwrap();
 
                 // If the window system is telling us to redraw, let's get our next swapchain image
                 let frame = surface
@@ -565,6 +641,8 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     // we want to draw onto our swapchain texture view (that's where the colors will go)
                     // and that there's no depth buffer or stencil buffer.
 
+                    text_renderer.render(&atlas, &mut rpass).unwrap();
+
                     rpass.set_pipeline(&render_pipeline);
                     rpass.set_bind_group(0, &sprite_bind_group, &[]);
                     rpass.set_bind_group(1, &texture_bind_group, &[]);
@@ -580,6 +658,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 // Then we wait for the commands to finish and tell the windowing system to
                 // present the swapchain image.
                 frame.present();
+                atlas.trim();
                 window.request_redraw();
             }
 
@@ -643,64 +722,40 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
                 sprites[acorn.sprites_index].screen_region = acorn.screen_region;
 
-                let acorn_width: f32 = sprites[acorn.sprites_index].screen_region[2];
-                let squirrel_width: f32 = sprites[squirrel.sprites_index].screen_region[2];
-                let squirrel_x: f32 = sprites[squirrel.sprites_index].screen_region[0];
-                let squirrel_y: f32 = sprites[squirrel.sprites_index].screen_region[1];
                 let acorn_x: f32 = sprites[acorn.sprites_index].screen_region[0];
                 let acorn_y: f32 = sprites[acorn.sprites_index].screen_region[1];
+                let acorn_width: f32 = sprites[acorn.sprites_index].screen_region[2];
                 let acorn_height: f32 = sprites[acorn.sprites_index].screen_region[3];
+
+                let mut squirrel_x: f32 = sprites[squirrel.sprites_index].screen_region[0];
+                let squirrel_y: f32 = sprites[squirrel.sprites_index].screen_region[1];
+                let mut squirrel_width: f32 = sprites[squirrel.sprites_index].screen_region[2];
                 let squirrel_height: f32 = sprites[squirrel.sprites_index].screen_region[3];
-                // check for collisions
-                if ((acorn_x + acorn_width > squirrel_x) && (acorn_x < squirrel_x + squirrel_width)) 
-                &&  ((acorn_y - acorn_height < squirrel_y) && (acorn_y > squirrel_y - squirrel_height)) {
-                    nut_count +=1;
 
-
-                    let mouse_pos = input.mouse_pos();
-                    let (mouse_x_norm, mouse_y_norm) = ((mouse_pos.x / config.width as f64),
-                                                        (mouse_pos.y / config.height as f64));
-                    // Load a font.
-                    let font = Vec::from(include_bytes!("../src/ac-thermes.ttf") as &[u8]);
-                    // /Users/Aniku/cs181g/5-interactive-drawing/src/ac-thermes.ttf
-                    let font = Font::try_from_vec(font).unwrap();
-
-                    let font_size = 40.0;
-                    let scale = Scale {
-                        x: 50.0,
-                        y: 50.0,
-                    };
-                    imageproc::drawing::draw_text_mut(
-                            &mut img_bg,
-                            color,
-                            (mouse_x_norm * (img_bg_w as f64)) as i32, 
-                            (mouse_y_norm * (img_bg_h as f64)) as i32, 
-                            scale, 
-                            &font, 
-                            &"WE HIT!"
-                            );
-                    queue.write_texture(
-                        tex_bg.as_image_copy(),
-                        &img_bg,
-                        wgpu::ImageDataLayout {
-                            offset: 0,
-                            bytes_per_row: Some(4 * img_bg_w),
-                            rows_per_image: Some(img_bg_h),
-                        },
-                        wgpu::Extent3d {
-                            width:img_bg_w,
-                            height:img_bg_h,
-                            depth_or_array_layers: 1,
-                        },
-                    );
-
-
-                    println!("WE HIT!\n");
-                    acorn.speed += 0.2;
-                    acorn.reset_y();
+                // adjusting for right facing squirrel
+                if squirrel.facing_right {
+                    squirrel_x += squirrel_width;
+                    squirrel_width *= -1.0;
                 }
 
+                // Check for collisions
+                if (acorn_x + acorn_width > squirrel_x) && (acorn_x < squirrel_x + squirrel_width)
+                    && (acorn_y - acorn_height < squirrel_y) && (acorn_y > squirrel_y - squirrel_height) {
+                    // Collision detected, handle it here
+                    nut_count += 1;
+                    acorn.speed += 0.1;
+                    acorn.reset_y();
 
+                    if !gs.score_changing{
+                        gs.score += 1;
+                        let score_text = format!("Score: {}", gs.score);
+                        // buffer.set_text(&mut font_system, &gs.score.to_string(), Attrs::new().family(Family::SansSerif), Shaping::Advanced);    
+                        buffer.set_text(&mut font_system, &score_text, Attrs::new().family(Family::SansSerif), Shaping::Advanced);
+                        gs.score_changing = true;
+                    }
+
+                }
+                else{gs.score_changing = false;}
 
                 window.request_redraw();
             }
